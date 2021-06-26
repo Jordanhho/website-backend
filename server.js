444dc5fd5f7970d108f4955bc1da0fc5d77a33e6
Jordan <jordanhho12@hotmail.com>
2021-06-25 17:42:04 -0700

    test

--- server.js
+++ server.js
@@ -108,7 +108,8 @@ app.use(
             directives: {
                 ...helmet.contentSecurityPolicy.getDefaultDirectives(),
                 "img-src": ["'self'", "https://private-personal-website-storage.s3.us-west-2.amazonaws.com/", "https://dkbz0bts1nczj.cloudfront.net/"],
-                "script-src": ["'self'", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/"]
+                "script-src": ["'self'", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/"],
+                "frame-src": ["'self'", "https://www.google.com/"],
             },
         },
     })
