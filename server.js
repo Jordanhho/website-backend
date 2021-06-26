7785eac4b94f64d8dbd85755b820fd579e96aea9
Jordan <jordanhho12@hotmail.com>
2021-06-25 17:40:31 -0700

    Add google recaptcha to exclusions

--- server.js
+++ server.js
@@ -108,6 +108,7 @@ app.use(
             directives: {
                 ...helmet.contentSecurityPolicy.getDefaultDirectives(),
                 "img-src": ["'self'", "https://private-personal-website-storage.s3.us-west-2.amazonaws.com/", "https://dkbz0bts1nczj.cloudfront.net/"],
+                "script-src": ["'self'", "https://www.google.com/recaptcha/"]
             },
         },
     })
