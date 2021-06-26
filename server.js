7b5c9db749736defd42f7d8b7b0583251fd76627
Jordan <jordanhho12@hotmail.com>
2021-06-25 17:39:11 -0700

    test

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.use(
         contentSecurityPolicy: {
             directives: {
                 ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-                "img-src": ["'self'", "https://private-personal-website-storage.s3.us-west-2.amazonaws.com/"],
+                "img-src": ["'self'", "https://private-personal-website-storage.s3.us-west-2.amazonaws.com/", "https://dkbz0bts1nczj.cloudfront.net/"],
             },
         },
     })
