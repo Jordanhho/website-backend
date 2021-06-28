d9d7abd6b7537dc953af95f6070c52cc49f5ecaa
Jordan <jordanhho12@hotmail.com>
2021-06-28 00:51:46 -0700

    hotfix

--- server.js
+++ server.js
@@ -143,6 +143,8 @@ app.use(
                 "img-src": ["'self'", "https://private-personal-website-storage.s3.us-west-2.amazonaws.com/", "https://dkbz0bts1nczj.cloudfront.net/"],
                 "script-src": ["'self'", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://csgo-app.jordanho.ca/", "https://jordanho.ca/"],
                 "frame-src": ["'self'", "https://www.google.com/"],
+                "default-src": ["'self'", "https://csgo-app.jordanho.ca/", "https://jordanho.ca/"],
+                "connect-src": ["'self'", "https://csgo-app.jordanho.ca/", "https://jordanho.ca/"],
             },
         },
     })
